a6c63905b2dffccb2089eb7031e3bd2af2d5cbb3
Kirankumar <kiran.bolaniya@mobiosolutions.com>
2023-08-28 15:37:24 +0530

    Added Model with ObjectId field

--- scripts/model_script.sh
+++ scripts/model_script.sh
@@ -14,17 +14,30 @@ MODEL_CONTENT="const mongoose = require('mongoose');\n\n"
 MODEL_CONTENT+="const ${MODEL_NAME}Schema = new mongoose.Schema({\n"
 
 # Loop through the remaining arguments and generate fields
-for FIELD_ARG in "$@"; do
+while [[ $# -gt 0 ]]; do
+    FIELD_ARG="$1"
+    shift  # Move to the next argument
+
+    # Split the FIELD_ARG into field name, field type, and possibly reference
     IFS=':' read -ra FIELD_INFO <<< "$FIELD_ARG"
     FIELD_NAME="${FIELD_INFO[0]}"
     FIELD_TYPE="${FIELD_INFO[1]}"
-    MODEL_CONTENT+="    $FIELD_NAME: { type: $FIELD_TYPE },\n"
+    REF_PROPERTY=""
+
+    # If there's a reference, adjust properties accordingly
+    if [[ "${FIELD_INFO[2]}" == "ref" ]]; then
+        REF_MODEL="${FIELD_INFO[3]}"
+        FIELD_TYPE="mongoose.Schema.Types.ObjectId"
+        REF_PROPERTY="ref: '$REF_MODEL',"
+    fi
+
+    MODEL_CONTENT+="    $FIELD_NAME: { type: $FIELD_TYPE, $REF_PROPERTY },\n"
 done
 
 MODEL_CONTENT+="}, { timestamps: true });\n\n"
-MODEL_CONTENT+=`echo "module.exports = mongoose.model('${CAPITALIZED_MODEL_NAME}', ${MODEL_NAME}Schema);"`
+MODEL_CONTENT+="module.exports = mongoose.model('${CAPITALIZED_MODEL_NAME}', ${MODEL_NAME}Schema);"
 
 # Write the model content to the file
 echo -e "$MODEL_CONTENT" > "app/models/${MODEL_NAME}.model.js"
 
-echo "Express model ${MODEL_NAME} created successfully!"
+echo "Express model ${MODEL_NAME} created successfully!" with ObjectId field
